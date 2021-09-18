e4e4ed635ab1664035f9bd127cbe3ab6b1d65e0a
sshee0123 <55376366+sshee0123@users.noreply.github.com>
2021-09-18 11:19:43 +0900

    210918 07.py

---
+++ 07.py
@@ -0,0 +1,16 @@
+#구현 - 07 럭키 스트레이트
+
+n = input()
+length = len(n)
+summary = 0
+
+for i in range(length // 2):
+    summary += int(n[i])
+
+for i in range(length // 2, length):
+    summary -= int(n[i])
+
+if summary == 0:
+    print("LUCKY")
+else:
+    print("READY")
